8f5c058f7f90097a908f79c650ae03de26b4802e
Ivanner Mora <ivanner@gmail.com>
2021-06-07 12:10:31 -0500

    Added filter class parameter and summary line

--- resources/views/filters/dropdown-filter.blade.php
+++ resources/views/filters/dropdown-filter.blade.php
@@ -1,4 +1,4 @@
-<select class="form-control" name="filters[{{ $name }}]" v-model="filters['{{ $name }}']" v-on:change="filter(true)">
+<select class="{{ $cssClass }}" name="filters[{{ $name }}]" v-model="filters['{{ $name }}']" v-on:change="filter(true)">
     <option value=""></option>
     @foreach ($items as $k => $v)
         <option value="{{ $k }}">{{ $v }}</option>
